456dd220259ed8beefbf9a63bd317f542e5ed506
oatfazwaz <93711042+oatfazwaz@users.noreply.github.com>
2022-06-07 11:08:10 +0700

    yarn install force

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import GithubProvider from "next-auth/providers/github"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
+// import GoogleProvider from "next-auth/providers/google"
+// import FacebookProvider from "next-auth/providers/facebook"
+// import GithubProvider from "next-auth/providers/github"
+// import TwitterProvider from "next-auth/providers/twitter"
+// import Auth0Provider from "next-auth/providers/auth0"
 // import AppleProvider from "next-auth/providers/apple"
 // import EmailProvider from "next-auth/providers/email"
 import CredentialsProvider from "next-auth/providers/credentials"
@@ -22,6 +22,7 @@ interface IToken {
 }
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
+// @ts-ignore
 export default NextAuth({
     // https://next-auth.js.org/configuration/providers/oauth
     providers: [
@@ -42,27 +43,27 @@ export default NextAuth({
           },
         }),
         */
-        FacebookProvider({
-            clientId: process.env.FACEBOOK_ID,
-            clientSecret: process.env.FACEBOOK_SECRET,
-        }),
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
-        }),
-        TwitterProvider({
-            clientId: process.env.TWITTER_ID,
-            clientSecret: process.env.TWITTER_SECRET,
-        }),
-        Auth0Provider({
-            clientId: process.env.AUTH0_ID,
-            clientSecret: process.env.AUTH0_SECRET,
-            issuer: process.env.AUTH0_ISSUER,
-        }),
+        // FacebookProvider({
+        //     clientId: process.env.FACEBOOK_ID,
+        //     clientSecret: process.env.FACEBOOK_SECRET,
+        // }),
+        // GithubProvider({
+        //     clientId: process.env.GITHUB_ID,
+        //     clientSecret: process.env.GITHUB_SECRET,
+        // }),
+        // GoogleProvider({
+        //     clientId: process.env.GOOGLE_ID,
+        //     clientSecret: process.env.GOOGLE_SECRET,
+        // }),
+        // TwitterProvider({
+        //     clientId: process.env.TWITTER_ID,
+        //     clientSecret: process.env.TWITTER_SECRET,
+        // }),
+        // Auth0Provider({
+        //     clientId: process.env.AUTH0_ID,
+        //     clientSecret: process.env.AUTH0_SECRET,
+        //     issuer: process.env.AUTH0_ISSUER,
+        // }),
         CredentialsProvider({
             // The name to display on the sign in form (e.g. 'Sign in with...')
             name: 'Credentials',
